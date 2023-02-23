4431d4d9454ca716982010bcfd603dc6d1314d1c
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-02-23 19:59:05 +0530

    initialize property routes

---
+++ routes/property.routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import {
+    getAllProperties, getPropertyDetail, createProperty, updateProperty, deleteProperty,
+} from '../controllers/property.controller.js';
+
+const router = express.Router();
+
+router.route('/').get(getAllProperties);
+router.route('/:id').get(getPropertyDetail);
+router.route('/').post(createProperty);
+router.route('/:id').patch(updateProperty);
+router.route('/:id').delete(getAllProperties);
+
+export default router;
