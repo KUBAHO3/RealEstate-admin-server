0e6a8cb5966b538824ba94cd0f47d3e8f01d4410
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-02-23 23:04:51 +0530

    add create user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,28 @@
 import User from '../mongodb/models/user.js';
 
 const getAllUsers = async (req, res) => {};
-const createUser = async (req, res) => {};
+
+const createUser = async (req, res) => {
+    try {
+        const {name, email, avatar} = req.body;
+    
+        const userExists = await User.findOne({ email });
+    
+        if (userExists) return res.status(200).json(userExists);
+    
+        const newUser = await User.create({
+            name,
+            email,
+            avatar,
+        });
+    
+        res.status(200).json(newUser);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
 const getUserInfoByID = async (req, res) => {};
 
 export {
