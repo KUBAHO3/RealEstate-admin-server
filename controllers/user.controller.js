c8a5ee87c0ed373d10a6604821fb9dd85ebc6eda
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-04 17:01:20 +0530

    add getUserInfoById

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -31,7 +31,21 @@ const createUser = async (req, res) => {
     }
 };
 
-const getUserInfoByID = async (req, res) => {};
+const getUserInfoByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findOne({ _id: id }).populate("allProperties");
+
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 export {
     getAllUsers,
